79b4e765233adde65628954e63fca9aa6334a065
Hearse <28695977+HearseDev@users.noreply.github.com>
2024-02-11 14:40:00 -0800

    Future proof libcrane.h to conform to libroot

--- libCrane.h
+++ libCrane.h
@@ -1,25 +1,31 @@
 #import <dlfcn.h>
-// run this function to access the CraneManager class from processes other than SpringBoard
-__attribute__ ((unused)) static void loadLibCrane()
-{
-	dlopen("/usr/lib/libcrane.dylib", RTLD_NOW);
+#import <libroot.h>
+// run this function to access the CraneManager class from processes other than
+// SpringBoard
+__attribute__((unused)) static void loadLibCrane() {
+  dlopen(JBROOT_PATH_CSTRING("/usr/lib/libcrane.dylib"), RTLD_NOW);
 }
 
-// make a class that conforms to this protocol and add it to CraneManager using the addObserver: method
+// make a class that conforms to this protocol and add it to CraneManager using
+// the addObserver: method
 @protocol CraneManagerObserver
 
 @optional
 - (void)didChangeRootSettings;
-- (void)didChangeSettingsForApplicationWithIdentifier:(NSString*)applicationID;
-- (void)didChangeContainersOfApplicationWithIdentifier:(NSString*)applicationID;
-- (void)didChangeContainerSettingsForContainerWithIdentifier:(NSString*)containerID onApplicationWithIdentifier:(NSString*)applicationID;
+- (void)didChangeSettingsForApplicationWithIdentifier:(NSString *)applicationID;
+- (void)didChangeContainersOfApplicationWithIdentifier:
+    (NSString *)applicationID;
+- (void)didChangeContainerSettingsForContainerWithIdentifier:
+            (NSString *)containerID
+                                 onApplicationWithIdentifier:
+                                     (NSString *)applicationID;
 
 @end
 
 typedef NS_ENUM(NSInteger, ContainerPathType) {
-    ContainerPathTypeApp,
-    ContainerPathTypeGroup,
-	ContainerPathTypePlugin
+  ContainerPathTypeApp,
+  ContainerPathTypeGroup,
+  ContainerPathTypePlugin
 };
 
 @interface CraneManager : NSObject
@@ -27,49 +33,91 @@ typedef NS_ENUM(NSInteger, ContainerPathType) {
 + (instancetype)sharedManager;
 
 // prefs
-- (NSDictionary*)preferencesCopy;
-- (void)setPreferenceValue:(id)value forKey:(NSString*)key;
-- (void)removePreferenceValueForKey:(NSString*)key;
-- (id)preferenceValueForKey:(NSString*)key;
+- (NSDictionary *)preferencesCopy;
+- (void)setPreferenceValue:(id)value forKey:(NSString *)key;
+- (void)removePreferenceValueForKey:(NSString *)key;
+- (id)preferenceValueForKey:(NSString *)key;
 
 // observers
-- (void)addObserver:(NSObject<CraneManagerObserver>*)observer;
-- (void)removeObserver:(NSObject<CraneManagerObserver>*)observer;
+- (void)addObserver:(NSObject<CraneManagerObserver> *)observer;
+- (void)removeObserver:(NSObject<CraneManagerObserver> *)observer;
 
 // app settings
-- (BOOL)isApplicationSupportedByCrane:(NSString*)applicationID;
-- (NSArray*)identfiersOfApplicationsThatHaveNonDefaultContainers;
-- (NSArray*)identifiersOfAllSupportedApplications;
-- (NSString*)displayNameForApplicationWithIdentifier:(NSString*)applicationID;
-- (NSDictionary*)applicationSettingsForApplicationWithIdentifier:(NSString*)applicationID;
-- (void)setApplicationSettings:(NSDictionary*)appSettings forApplicationWithIdentifier:(NSString*)applicationID;
-- (NSString*)activeContainerIdentifierForApplicationWithIdentifier:(NSString*)applicationID;
-- (void)setActiveContainerIdentifier:(NSString*)containerID forApplicationWithIdentifier:(NSString*)applicationID; // does not respect biometric protection setting
-- (void)setActiveContainerIdentifier:(NSString*)containerID forApplicationWithIdentifier:(NSString*)applicationID usingBiometricsIfNeededWithSuccessHandler:(void (^)(BOOL))successHandler; //respects biometric protection setting and does authentication if neccessary
-- (NSArray*)containerIdentifiersOfApplicationWithIdentifier:(NSString*)applicationID;
+- (BOOL)isApplicationSupportedByCrane:(NSString *)applicationID;
+- (NSArray *)identfiersOfApplicationsThatHaveNonDefaultContainers;
+- (NSArray *)identifiersOfAllSupportedApplications;
+- (NSString *)displayNameForApplicationWithIdentifier:(NSString *)applicationID;
+- (NSDictionary *)applicationSettingsForApplicationWithIdentifier:
+    (NSString *)applicationID;
+- (void)setApplicationSettings:(NSDictionary *)appSettings
+    forApplicationWithIdentifier:(NSString *)applicationID;
+- (NSString *)activeContainerIdentifierForApplicationWithIdentifier:
+    (NSString *)applicationID;
+- (void)setActiveContainerIdentifier:(NSString *)containerID
+        forApplicationWithIdentifier:
+            (NSString *)
+                applicationID; // does not respect biometric protection setting
+- (void)setActiveContainerIdentifier:(NSString *)containerID
+                 forApplicationWithIdentifier:(NSString *)applicationID
+    usingBiometricsIfNeededWithSuccessHandler:
+        (void (^)(BOOL))successHandler; // respects biometric protection setting
+                                        // and does authentication if neccessary
+- (NSArray *)containerIdentifiersOfApplicationWithIdentifier:
+    (NSString *)applicationID;
 
 // container management
-- (void)createNewContainerWithName:(NSString*)containerName andIdentifier:(NSString*)containerID forApplicationWithIdentifier:(NSString*)applicationID;
-- (NSString*)createNewContainerWithName:(NSString*)containerName forApplicationWithIdentifier:(NSString*)applicationID; // returns containerIdentifier
-- (void)deleteContainerWithIdentifier:(NSString*)containerID forApplicationWithIdentifier:(NSString*)applicationID;
-- (void)wipeContainerWithIdentifier:(NSString*)containerIdD forApplicationWithIdentifier:(NSString*)applicationID shouldRepopulate:(BOOL)repopulate;
-- (NSString*)makeDefaultForContainerWithIdentifier:(NSString*)containerID forApplicationWithIdentifier:(NSString*)applicationID;
+- (void)createNewContainerWithName:(NSString *)containerName
+                     andIdentifier:(NSString *)containerID
+      forApplicationWithIdentifier:(NSString *)applicationID;
+- (NSString *)createNewContainerWithName:(NSString *)containerName
+            forApplicationWithIdentifier:
+                (NSString *)applicationID; // returns containerIdentifier
+- (void)deleteContainerWithIdentifier:(NSString *)containerID
+         forApplicationWithIdentifier:(NSString *)applicationID;
+- (void)wipeContainerWithIdentifier:(NSString *)containerIdD
+       forApplicationWithIdentifier:(NSString *)applicationID
+                   shouldRepopulate:(BOOL)repopulate;
+- (NSString *)makeDefaultForContainerWithIdentifier:(NSString *)containerID
+                       forApplicationWithIdentifier:(NSString *)applicationID;
 
 // containers settings
-- (NSDictionary*)containerSettingsForContainerWithIdentifier:(NSString*)containerID ofApplicationWithIdentifier:(NSString*)applicationID;
-- (void)setContainerSettings:(NSDictionary*)containerSettings forContainerWithIdentifier:(NSString*)containerID ofApplicationWithIdentifier:(NSString*)applicationID;
-- (void)enumerate:(void (^)(ContainerPathType type, NSString* identifier, NSString* path))block pathsAssociatedToContainerWithIdentifier:(NSString*)containerID ofApplicationWithIdentifier:(NSString*)applicationID;
-- (NSDictionary*)pathsAssociatedToContainerWithIdentifier:(NSString*)containerID ofApplicationWithIdentifier:(NSString*)applicationID;
-- (void)sizeOccupiedByContainerWithIdentifier:(NSString*)containerID ofApplicationWithIdentifier:(NSString*)applicationID completionHandler:(void (^)(uint64_t))completionHandler;
-- (NSString*)displayNameForContainerWithName:(NSString*)containerName isDefaultContainer:(BOOL)isDefault shouldUseShortVersion:(BOOL)shortVersion;
-- (NSString*)displayNameForContainerWithIdentifier:(NSString*)containerID ofApplicationWithIdentifier:(NSString*)applicationID shouldUseShortVersion:(BOOL)shortVersion;
+- (NSDictionary *)
+    containerSettingsForContainerWithIdentifier:(NSString *)containerID
+                    ofApplicationWithIdentifier:(NSString *)applicationID;
+- (void)setContainerSettings:(NSDictionary *)containerSettings
+     forContainerWithIdentifier:(NSString *)containerID
+    ofApplicationWithIdentifier:(NSString *)applicationID;
+- (void)enumerate:(void (^)(ContainerPathType type, NSString *identifier,
+                            NSString *path))block
+    pathsAssociatedToContainerWithIdentifier:(NSString *)containerID
+                 ofApplicationWithIdentifier:(NSString *)applicationID;
+- (NSDictionary *)
+    pathsAssociatedToContainerWithIdentifier:(NSString *)containerID
+                 ofApplicationWithIdentifier:(NSString *)applicationID;
+- (void)sizeOccupiedByContainerWithIdentifier:(NSString *)containerID
+                  ofApplicationWithIdentifier:(NSString *)applicationID
+                            completionHandler:
+                                (void (^)(uint64_t))completionHandler;
+- (NSString *)displayNameForContainerWithName:(NSString *)containerName
+                           isDefaultContainer:(BOOL)isDefault
+                        shouldUseShortVersion:(BOOL)shortVersion;
+- (NSString *)displayNameForContainerWithIdentifier:(NSString *)containerID
+                        ofApplicationWithIdentifier:(NSString *)applicationID
+                              shouldUseShortVersion:(BOOL)shortVersion;
 
 // name to display in notifications
-- (NSString*)displayNameForContainerInNotificationWithIdentifier:(NSString*)containerID ofApplicationWithIdentifier:(NSString*)applicationID;
-- (NSString*)containerNameToDisplayInNotificationWithUserInfoOrContext:(NSDictionary*)userInfoOrContext ofApplicationWithIdentifier:(NSString*)applicationID;
+- (NSString *)displayNameForContainerInNotificationWithIdentifier:
+                  (NSString *)containerID
+                                      ofApplicationWithIdentifier:
+                                          (NSString *)applicationID;
+- (NSString *)containerNameToDisplayInNotificationWithUserInfoOrContext:
+                  (NSDictionary *)userInfoOrContext
+                                            ofApplicationWithIdentifier:
+                                                (NSString *)applicationID;
 
 // SpringBoard
-- (void)flushCFPrefsdCacheForApplicationWithIdentifier:(NSString*)applicationID;
-- (void)reloadApplicationWithIdentifier:(NSString*)applicationID;
+- (void)flushCFPrefsdCacheForApplicationWithIdentifier:
+    (NSString *)applicationID;
+- (void)reloadApplicationWithIdentifier:(NSString *)applicationID;
 
 @end
